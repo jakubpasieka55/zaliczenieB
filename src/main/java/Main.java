5feed1d3e44c1a4456459f85585cf0a822a7e6d1
jakubpasieka55 <177260@stud.prz.edu.pl>
2025-05-21 14:14:01 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,1 +1,9 @@
-TU BĘDZIE POLECENIE STAY TUNED
+Napisz program w języku Java, który:
+
+1. Wczyta od użytkownika ciąg znaków i zapisze go jako tablicę znaków.
+2. Wczyta dwa indeksy (liczby całkowite), które mają zostać zamienione.
+3. Zamieni miejscami znaki znajdujące się na tych pozycjach w tablicy.
+
+    Jeśli którykolwiek indeks jest nieprawidłowy (poza zakresem), należy wyświetlić komunikat o błędzie.
+4. Wyświetli zmodyfikowaną tablicę jako tekst
+  
